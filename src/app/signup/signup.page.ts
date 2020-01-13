eacf6a701da238bc250da5bd6095675595879fab
gigigimay <mo.maytiya_st@tni.ac.th>
2020-01-13 09:15:01 +0700

    finish signup page

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController, AlertController, NavController } from '@ionic/angular';
+import { Subscription } from 'rxjs'; // for retreive data and unsubscrib() data
+
+import { AuthService } from '../auth.service'; // model
+import { FeedBack } from '../models/feedback'; // service
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +12,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupPage implements OnInit {
 
-  constructor() { }
+  sub: Subscription;
+  feedback: FeedBack;
+
+  constructor(
+    private authService: AuthService,
+    private loadingCtrl: LoadingController,
+    private alertCtrl: AlertController,
+    private navCtrl: NavController
+  ) { }
 
   ngOnInit() {
   }
 
+  async signup(value) {
+    const { fullname, email, password } = value;
+
+    const loading = await this.loadingCtrl.create({
+      spinner: 'bubbles',
+      message: 'Saving data'
+    });
+    await loading.present();
+
+    this.authService.signup(fullname, email, password).subscribe(
+      async (f) => {
+        this.feedback = f;
+        const { message } = this.feedback;
+        const alert = await this.alertCtrl.create({
+          message,
+          buttons: ['Ok']
+        });
+        await alert.present();
+        this.navCtrl.navigateRoot('/');
+      },
+      async (e) => {
+        await loading.dismiss();
+      },
+      async () => { // dissmiss the Loading when finish fetching data
+        await loading.dismiss();
+      }
+    );
+  }
 }
